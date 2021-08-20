2b260da7e2928c91316fed1ed36f4face4882785
vchavarria1995 <vchavarria1995@gmail.com>
2021-08-20 14:58:28 -0500

    bubblepage tests passing

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -1,13 +1,70 @@
 import React from 'react';
 import MutationObserver from 'mutationobserver-shim';
-
-import { render, screen} from "@testing-library/react";
+import {fetchColorService as mockFetch} from '../services/fetchColorService'
+import { render, screen, waitFor} from "@testing-library/react";
 import BubblePage from './BubblePage';
 
-test("Renders without errors", ()=> {
+jest.mock("../services/fetchColorService")
+
+const Colors = {
+    data: [
+        {
+            color: 'aliceblue',
+            code: {
+                hex: '#f0f8ff',
+            },
+            id: 1,
+        },
+        {
+            color: 'limegreen',
+            code: {
+                hex: '#99ddbc',
+            },
+            id: 2,
+        },
+        {
+            color: 'aquamarine',
+            code: {
+                hex: '#7fffd4',
+            },
+            id: 3,
+        },
+        {
+            color: 'lilac',
+            code: {
+                hex: '#9a99dd',
+            },
+            id: 4,
+        },
+        {
+            color: 'softpink',
+            code: { 
+                hex: '#dd99ba',
+            },
+            id: 5,
+        },
+    ]
+}
+
+test("Renders without errors", async ()=> {
+    mockFetch.mockResolvedValueOnce([])
+    render(<BubblePage/>)
+    await waitFor(() => {
+        const Colors = screen.getByText(/colors/i)
+        const bubbles = screen.getByText(/bubbles/i)
+        expect(Colors).toHaveTextContent('colors')
+        expect(bubbles).toHaveTextContent('Bubbles')
+    })
     
 });
 
 test("Renders appropriate number of colors passed in through mock", async ()=> {
     //Keep in mind that our service is called on mount for this component.
+    mockFetch.mockResolvedValueOnce(Colors);
+    render(<BubblePage />);
+    await waitFor(() => []);
+    const list = screen.getAllByTestId('color');
+    const lilac = screen.getByText(/limegreen/i);
+    expect(list).toHaveLength(5);
+    expect(lilac).toBeInTheDocument();
 });
